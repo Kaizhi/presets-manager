558d7f5a7a47f79e4c97e80fe20e7490b9f35a30
Kaizhi Wei <kaizhi@yahoo-inc.com>
2015-06-14 18:00:17 -0700

    add dragdrop

--- component/main/Install.js
+++ component/main/Install.js
@@ -1,11 +1,24 @@
 var React = require( 'react' );
 var Router = require('react-router');
 var RouteHandler = Router.RouteHandler;
+var Dropzone = require('react-dropzone');
 
 var Install = React.createClass({
 	displayName: 'Install',
+	onDrop: function (files) {
+		console.log('Received files: ', files);
+	},
+	onHover: function () {
+		console.log('hover');
+	},
 	render: function() {
-		return <h3>Install</h3>;
+		return (
+		    <div>
+		      <Dropzone onDrop={this.onDrop} style={{}}>
+		        <div>Try dropping some files here, or click to select files to upload.</div>
+		      </Dropzone>
+		    </div>
+		);
 	}
 });
 
