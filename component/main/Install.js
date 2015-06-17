667c749c51aba9636e26f32ab735801b9a229dff
Kaizhi Wei <kaizhi@yahoo-inc.com>
2015-06-17 00:08:02 -0700

    extract zip file and copy .lrtemplate files to the lightroom develop presets dir on osx

--- component/main/Install.js
+++ component/main/Install.js
@@ -6,24 +6,59 @@ var Dropzone = require('react-dropzone');
 var unzip = window.require('unzip2');
 var fs = window.require('fs');
 var path = window.require('path');
-
-window.fs = fs;
+var osenv = window.require('osenv');
+var os = window.require('os');
+var mkdirp = window.require('mkdirp');
 
 var Install = React.createClass({
 	displayName: 'Install',
+	getDefaultProps: function () {
+		var presetsPath;
+
+		if (os.platform() === 'darwin') { //OSX
+			presetsPath = path.join(osenv.home(), '/Library/Application Support/Adobe/Lightroom/Develop Presets');
+		} else {
+			presetsPath = '';
+		}
+		return {
+			presetsPath: presetsPath
+		};
+	},
+	componentDidMount: function () {
+		console.log(osenv.tmpdir(), this.props);
+	},
 	onDrop: function (files) {
+		var component = this,
+			baseName;
+
 		files.forEach(function (file) {
-			console.log(path.dirname(file.path));
-			fs.createReadStream(file.path).pipe(unzip.Extract({ path: path.dirname(file.path) }));
+			// Get the name of the subdirectory that we will unzip the files to
+			baseName = path.basename(file.path, '.zip');
+
+			fs.createReadStream(file.path).pipe(unzip.Parse())
+			.on('entry', function (entry) {
+				// If this is a lrtemplate file, we can copy it to the right directory
+				if (path.extname(entry.path) === '.lrtemplate') {
+					// Create the subdirectory under the lightroom develop presets dir if it doesn't exist
+					mkdirp(path.join(component.props.presetsPath, baseName), function (err) {
+						if (err) {
+							return; //handle error
+						} else {
+							// Extract lrtemplate files to the directory
+							entry.pipe(fs.createWriteStream(path.join(component.props.presetsPath, baseName, entry.path)));
+						}
+					});
+				} else {
+				    entry.autodrain();
+				}
+			});
 		});
 	},
 	render: function() {
 		return (
-		    <div>
-		    	<Dropzone onDrop={this.onDrop} style={{}}>
-		        	<div>Try dropping some files here, or click to select files to upload.</div>
-		      	</Dropzone>
-		    </div>
+	    	<Dropzone onDrop={this.onDrop} style={{}}>
+	        	<div>Try dropping some files here, or click to select files to upload.</div>
+	      	</Dropzone>
 		);
 	}
 });
