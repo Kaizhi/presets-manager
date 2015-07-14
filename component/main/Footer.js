bbd46299557a34139a9354d113d0593d176cef18
Kaizhi Wei <kaizhi@yahoo-inc.com>
2015-07-13 23:29:53 -0700

    change to tray style app

--- component/main/Footer.js
+++ component/main/Footer.js
@@ -1,14 +1,21 @@
 var React = require( 'react' );
+var ipc = window.require('ipc');
 
 var Footer = React.createClass({
-
+	closeApp: function () {
+		ipc.send('app:event', 'close');
+	},
 	render: function() {
 		return (
 	      	<footer>
-	      		<ul className="sections">
-	      			<li><span><i className="fa fa-question-circle"></i></span></li>
+	      		<ul className="actions primary">
+	      			<li onCLick={this.props.installPreset}><span><i className="fa fa-plus"></i></span></li>
+	      		</ul>
+	      		<ul className="actions secondary">
 	      			<li><span><i className="fa fa-folder-open-o"></i></span></li>
 	      			<li><span><i className="fa fa-refresh"></i></span></li>
+	      			<li><span><i className="fa fa-question-circle"></i></span></li>
+	      			<li onClick={this.closeApp}><span><i className="fa fa-times"></i></span></li>
 	      		</ul>
 	      	</footer>
 		);
